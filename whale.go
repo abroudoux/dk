082d41a689e5ae8fddb26fa266b1819469ed6ad5
arthur <arthur.broudoux@gmail.com>
2024-12-15 21:18:41 +0100

    feat: get docker containers Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- whale.go
+++ whale.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -14,6 +16,21 @@ func main() {
 	if !isDockerRunning() {
 		println("Docker is not running")
 		os.Exit(1)
+	}
+
+	if len(os.Args) > 1 {
+		flagMode()
+		os.Exit(0)
+	}
+
+	containers, err := getContainers()
+	if err != nil {
+		println("Error getting containers")
+		os.Exit(1)
+	}
+
+	for _, container := range containers {
+		fmt.Println(container)
 	}
 }
 
@@ -27,4 +44,30 @@ func isDockerRunning() bool {
 	cmd := exec.Command("docker", "container", "ls")
 	err := cmd.Run()
 	return err == nil
+}
+
+func flagMode() {
+	flag := os.Args[1]
+
+	switch flag {
+	case "--help", "-h":
+		printHelpManual()
+	case "--version", "-v":
+		fmt.Println("0.0.1")
+	}
+}
+
+func printHelpManual() {
+	fmt.Println("Usage: whale [options]")
+	fmt.Printf("  %-20s %s\n", "whale [--help | -h]", "Show this help message")
+}
+
+func getContainers() ([]string, error) {
+	cmd := exec.Command("docker", "container", "ls", "-a",)
+	containers, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Fields(string(containers)), nil
 }
