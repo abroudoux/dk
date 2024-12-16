b9a09e9c6f67301ff72346614216b013eb4d66a2
arthur <arthur.broudoux@gmail.com>
2024-12-16 11:30:06 +0100

    feat: get containers and display choice Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- whale.go
+++ whale.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
@@ -18,20 +20,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	if len(os.Args) > 1 {
-		flagMode()
-		os.Exit(0)
-	}
-
 	containers, err := getContainers()
 	if err != nil {
 		println("Error getting containers")
 		os.Exit(1)
 	}
 
-	for _, container := range containers {
-		fmt.Println(container)
+	if len(os.Args) > 1 {
+		flagMode(containers)
+		os.Exit(0)
 	}
+
+	containerSelected, err := chooseContainer(containers)
+	if err != nil {
+		println("Error choosing container", err)
+		os.Exit(1)
+	}
+
+	println("Container selected: ", containerSelected)
 }
 
 func isDockerInstalled() bool {
@@ -46,10 +52,18 @@ func isDockerRunning() bool {
 	return err == nil
 }
 
-func flagMode() {
+func flagMode(containers []string) {
 	flag := os.Args[1]
 
 	switch flag {
+	case "--run", "-r":
+		containerSelected, err := chooseContainer(containers)
+		if err != nil {
+			println("Error choosing container")
+			os.Exit(1)
+		}
+
+		println("Container selected: ", containerSelected)
 	case "--help", "-h":
 		printHelpManual()
 	case "--version", "-v":
@@ -63,11 +77,92 @@ func printHelpManual() {
 }
 
 func getContainers() ([]string, error) {
-	cmd := exec.Command("docker", "container", "ls", "-a",)
-	containers, err := cmd.Output()
+	cmd := exec.Command("docker", "container", "ls", "-a")
+	output, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
 
-	return strings.Fields(string(containers)), nil
+	lines := strings.Split(string(output), "\n")
+	var containers []string
+	for _, line := range lines {
+		if line != "" {
+			containers = append(containers, line)
+		}
+	}
+
+	return containers, nil
+}
+
+type containerChoice struct {
+	containers []string
+	cursor    int
+	selectedContainer string
+}
+
+func initialContainerModel(containers []string) containerChoice {
+	return containerChoice{
+		containers: containers,
+		cursor:    len(containers) - 1,
+		selectedContainer: "",
+	}
+}
+
+func (menu containerChoice) Init() tea.Cmd {
+	return nil
+}
+
+func (menu containerChoice) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			return menu, tea.Quit
+		case "up":
+			menu.cursor--
+			if menu.cursor < 0 {
+				menu.cursor = len(menu.containers) - 1
+			}
+		case "down":
+			menu.cursor++
+			if menu.cursor >= len(menu.containers) {
+				menu.cursor = 0
+			}
+		case "enter":
+			menu.selectedContainer = menu.containers[menu.cursor]
+			return menu, tea.Quit
+		}
+	}
+
+	return menu, nil
+}
+
+func (menu containerChoice) View() string {
+	s := "\033[H\033[2J"
+    s += "Choose a container:\n\n"
+
+	for i, container := range menu.containers {
+        cursor := " "
+
+        if menu.cursor == i {
+            // cursor = renderCursor()
+            cursor = ">"
+            s += fmt.Sprintf("%s %s\n", cursor, container)
+        } else {
+            s += fmt.Sprintf("%s %s\n", cursor, container)
+        }
+    }
+
+    return s
+}
+
+func chooseContainer(containers []string) (string, error) {
+	containersMenu := tea.NewProgram(initialContainerModel(containers))
+	finalModel, err := containersMenu.Run()
+	if err != nil {
+		return "", err
+	}
+
+	containerMenu := finalModel.(containerChoice)
+	return containerMenu.selectedContainer, nil
 }
